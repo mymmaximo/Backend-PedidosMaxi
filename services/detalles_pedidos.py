dea7d2cc83447e355fb67a0dee57be84594937ac
Maxi <maxgiesenow@gmail.com>
2026-09-18 11:03:36 -0300

    Cambios Dia 18/09/2026

--- services/detalles_pedidos.py
+++ services/detalles_pedidos.py
@@ -3,7 +3,7 @@ from sqlalchemy.sql import func
 from sqlalchemy.orm import Session
 from db.models.detalles_pedido import Detalles_Pedido, Detalles_Pedido_Crear
 from db.models.productos import Productos
-from backend_pedidos.db.models.registro_precios import RegistroPrecios
+from db.models.registro_precios import RegistroPrecios
 
 def get_detalle_pedido(
         db: Session, 
